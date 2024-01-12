3895152490e4eb8bbb3a1f6e812bb2a395fd80c3
Jesse Silva <jessepescador22@gmail.com>
2024-01-11 23:29:44 -0300

    fazendo alterações e aplicações no js

--- js/app.js
+++ js/app.js
@@ -122,12 +122,57 @@ cardapio.metodos = {
                     MEU_CARRINHO.push(item[0])
                 }
 
+                cardapio.metodos.mensagem('Item adicionado com sucesso', 'green')
+
                 $("#qntd-" + id).text(0);
+
+                cardapio.metodos.atualizarBadgeTotal();
 
             }
         }
 
 
+    },
+
+    // atualiza o badge de totais dos botões do Meu carrinho
+    atualizarBadgeTotal: () => {
+
+        let total = 0;
+
+        $.each(MEU_CARRINHO, (i, e) => {
+            total += e.qntd
+        })
+
+        if (total > 0) {
+            $(".botao-carrinho").removeClass('hidden');
+            $(".container-total-carrinho").removeClass('hidden');
+        }
+        else {
+            $(".botao-carrinho").addClass('hidden');
+            $(".container-total-carrinho").addClass('hidden');
+        }
+
+        $(".badge-total-carrinho").html(total);
+
+    },
+
+    // mensagens
+    mensagem: (texto, cor = 'red', tempo = 3500) => {
+
+        let id = Math.floor(Date.now() * Math.random()).toString();
+
+        let msg = `<div id="msg-${id}" class="animated fadeInDown toast ${cor}">${texto}</div>`
+
+        $("#container-mensagens").append(msg);
+
+        setTimeout(() => {
+
+            $("#msg-" + id).removeClass('fadeInDown');
+            $("#msg-" + id).addClass('fadeOutUp');
+            setTimeout(() => {
+                $("#msg-" + id).remove();
+            }, 800)
+        }, tempo)
     }
 
 
